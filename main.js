0ea4280a56fc7127649954f8853d17769c8f0ae3
YatharthDive <71880750+YatharthDive@users.noreply.github.com>
2021-11-09 15:42:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 Video=""
 status1=""
+objects=[]
 function preload(){
     Video=createVideo("video.mp4")
     Video.hide()
@@ -10,6 +11,28 @@ function setup(){
 }
 function draw(){
     image(Video,0,0,300,300)
+    if (status1 !="")
+    {objectDetector.detect(Video,gotResult)
+        R=random(255)
+        G=random(255)
+        B=random(255)
+    for (var index = 0; index < objects.length; index++) {
+        fill(R,G,B)
+        percent= floor(objects[index].confidence*100)
+        text(objects[index].label+" "+percent+"%",objects[index].x,objects[index].y)
+        noFill()
+        stroke(R,G,B)
+        rect(objects[index].x,objects[index].y,objects[index].width,objects[index].height)
+    }
+    
+    }
+}
+function gotResult(error,results){
+    if (error){console.error(error)}
+    else{
+        console.log(results)
+        objects=results;
+    }
 }
 function start(){
     objectDetector=ml5.objectDetector("cocossd",modelLoaded)
